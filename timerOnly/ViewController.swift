1c004425f9ac9f57e150af5427be34dcc665c2cc
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-09-20 17:17:32 +0900

    Initial Commit

---
+++ timerOnly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  timerOnly
+//
+//  Created by user on 2020/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
